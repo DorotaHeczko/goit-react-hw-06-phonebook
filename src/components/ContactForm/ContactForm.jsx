6281428e4add7df8bc7303dfbf7d71847431f00b
Dorota Heczko <117163329+DorotaHeczko@users.noreply.github.com>
2023-07-15 05:45:14 +0200

    redux

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,36 +1,26 @@
-import { Component } from 'react';
-import css from './ContactForm.module.css';
-import PropTypes from 'prop-types';
+import { useDispatch } from "react-redux";
+import css from "./ContactForm.module.css";
+import { addContact } from "redux/contactsSlice";
 
-export class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
+export const ContactForm = () => {
+  const dispatch = useDispatch();
+
+ 
+  const handleInputSubmit = (ev) => {
+    ev.preventDefault();
+    const form = ev.target;
+    const name = form.elements.name.value;
+    const number = form.elements.number.value;
+
+    dispatch(addContact(name, number));
+
+    form.reset();
   };
-
-  handleInputChange = e => {
-    const { name, value } = e.target;
-    this.setState(prevFormData => ({
-      ...prevFormData,
-      [name]: value,
-    }));
-  };
-
-  handleInputSubmit = e => {
-    e.preventDefault();
-    const { name, number } = this.state;
-
-    this.props.onSubmit(name, number);
-
-    this.setState({ name: '', number: '' });
-  };
-
-  render() {
-    const { name, number } = this.state;
+  
 
     return (
       <div>
-        <form onSubmit={this.handleInputSubmit} className={css.form}>
+        <form onSubmit={handleInputSubmit} className={css.form}>
           <label className={css.formLabel}>
             Name
             <input
@@ -39,8 +29,6 @@ export class ContactForm extends Component {
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
-              value={name}
-              onChange={this.handleInputChange}
               className={css.formInput}
             />
           </label>
@@ -53,8 +41,6 @@ export class ContactForm extends Component {
               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               required
-              value={number}
-              onChange={this.handleInputChange}
               className={css.formInput}
             />
           </label>
@@ -65,9 +51,6 @@ export class ContactForm extends Component {
         </form>
       </div>
     );
-  }
+  
 }
 
-ContactForm.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
-};
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,29 +1,19 @@
-import { Component } from 'react';
-import PropTypes from 'prop-types';
-import css from './Filter.module.css';
+import { useDispatch } from "react-redux";
+import { filterContact } from "redux/filtersSlice";
 
-export class Filter extends Component {
-  constructor(props) {
-    super(props);
-    this.handleInputChange = this.handleInputChange.bind(this);
-  }
-
-  handleInputChange(e) {
-    const { value } = e.target;
-    this.props.onChange(value);
-  }
-
-  render() {
-    return (
-      <>
-        <h3>Find contact by name</h3>
-        <input className={css.formInput} onChange={this.handleInputChange} />
-      </>
-    );
-  }
-}
+import css from "./Filter.module.css";
 
 
-Filter.propTypes = {
-  onChange: PropTypes.func.isRequired,
+export const Filter = () => {
+  const dispatch = useDispatch();
+
+  const handleInputChange = e => dispatch(filterContact(e.target.value));
+
+  return (
+    <>
+      <h3>Find contact by name</h3>
+           <input className={css.formInput} onChange={handleInputChange} />
+    </>
+  );
 };
+
